03284f93c02cc322364e242dc9357741b137dff0
FeirieO <feirie001@gmail>
2023-06-16 13:25:44 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,17 +8,17 @@ int main(void)
 {
 	int i, j;
 
-	for (j = 0; j < 100; j++)
+	for (j = 0; j < 99; j++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = 0; j < 99; j++)
 		{
-			if (i < j)
+			if (i < j && i != j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
