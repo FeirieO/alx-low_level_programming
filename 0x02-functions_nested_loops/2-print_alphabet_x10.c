cbe6947ab80b7ee27127cfad172fd4c814c4f32f
FeirieO <feirie001@gmail>
2023-06-20 12:12:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description 'print alphabets except q and e'
+ * print_alphabet_x10 - Make alphabet x10 times
  * Return: Always 0
  */
 
@@ -10,8 +9,8 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	i = 0;
 
+	i = 0;
 	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
