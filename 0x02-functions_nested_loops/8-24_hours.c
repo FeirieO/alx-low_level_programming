f809a5fd70efc8d608e95167a09d25844bba8990
FeirieO <feirie001@gmail>
2023-06-20 14:49:42 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - starting from 00:00 to 23:59
+ * Description 'print every minutes of the day
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	a = 0;
+
+	while (a < 24)
+	{
+		b = 0;
+		while (b < 60)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a / 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b / 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
+
+}
