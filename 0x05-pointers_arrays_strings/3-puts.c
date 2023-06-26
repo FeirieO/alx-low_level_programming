60221c1c38ada3e3b62e1f54673d99bc7534dadd
FeirieO <feirie001@gmail>
2023-06-26 14:53:19 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: The string to print
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	for (;*str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+
+}
