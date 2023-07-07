4cb03077eda84bb3138705d4a8780c630f9c0ce4
FeirieO <feirie001@gmail>
2023-07-07 01:46:28 -0700

    Static lIbrary

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: The string to get the length of
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
